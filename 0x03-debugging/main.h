401ec9f23f6cb5a1948a2faa2e3f8f498a9db372
Timothy Lagat <lagattim.tl@gmail.com>
2023-02-24 15:43:13 -0800

    Add betty style comments

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,11 @@
 
 #include <stdio.h>
 
+/**
+ * positive_or_negative - checks whether the input is +ve, -ve or zero
+ * @i: The integer to be checked
+ * return: 0
+ */
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
