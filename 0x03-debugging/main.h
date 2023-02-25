6ec0fb07d085f91ab103cced8c864d9568c9ed41
Timothy Lagat <lagattim.tl@gmail.com>
2023-02-24 16:11:54 -0800

    Add function headers to main.h, comment out loop in 1-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -8,6 +8,11 @@
  * @i: The integer to be checked
  * return: 0
  */
+
 void positive_or_negative(int i);
+
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif /* MAIN_H */
