306fac5a3b82e730d0eec7787c13c12690cc9291
Timothy Lagat <lagattim.tl@gmail.com>
2023-03-07 18:07:21 -0800

    add prototype

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -27,4 +27,13 @@ char *_memset(char *s, char b, unsigned int n);
  */
 char *_memcpy(char *dest, char *src, unsigned int n);
 
+/**
+ * _strchr - locate 1st occurrence of char in string and returns pointer to it
+ * @s: string to search
+ * @c: characer to find
+ * Return: pointer to the character found in string
+ */
+
+char *_strchr(char *s, char c)
+
 #endif
